a10846913a0a848becccd152d159f45e171699a3
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-01-20 13:46:26 +0100

    jpa 1 quest; first commit

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,11 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,41 +13,96 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 public class SchoolController {
 
     // TODO : get school repository by dependency injection
-
-    @GetMapping("/schools")
-    public String getAll(Model model) {
-
-        // TODO : find all schools
-
-        return "schools";
-    }
-
+	@Autowired
+	private SchoolRepository repository;
+	
+	
+	// read all ...
+	@GetMapping("/schools")
+	public String getAll(Model model) {
+		
+		// TODO : find all schools
+		model.addAttribute("schools", repository.findAll());
+		
+		return "schools";
+	}
+	
+	
+	// read one...
     @GetMapping("/school")
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
 
         // TODO : find one school by id
+    	School school = new School();
+    	if (id != null) {
+    		Optional<School> optionalSchool = repository.findById(id);
+    		if (optionalSchool.isPresent()) {
+    			school = optionalSchool.get();
+    		}
+    	}
+    	model.addAttribute("school", school);
 
         return "school";
     }
+    
+/* additional information out of the quest:    
+    
+    
+--> This is significantly different from the code that we have written before
+    particularly with the use of the methods: isPresent and get when calling for
+    repository.findById(id).
+    
+    The reason for this is as follows: findById does not return a Wizard but an Optional<Wizard>!
+    The class java.util.Optional "wraps" a class and adds (among other things) the following methods:
+    
+    - isPresent: returns true if an element has been found in the database, false if not
+    - get: if an element has been found, it returns its value, otherwise it raises an exception
+    
+    You will find additional information about the class Optional in the resources section.
+    When displaying a wizard, the following text will appear in the console of Spring:
+    
+*/
+    
+        
 
+    // create / update ...
     @PostMapping("/school")
     public String postSchool(@ModelAttribute School school) {
 
         // TODO : create or update a school
-
-        return "redirect:/schools";
+    	repository.save(school);
+    	return "redirect:/schools";
     }
+    
+/*  Additional INFO:  
+    
+    There are two possibilities when it comes to the POSTed Wizard wizard.
+    
+    - either it has an id of null and the wizard is created in a database
+    - or it already has an id and the data of the corresponding wizard is modified
+    
+*/    
+    
+    
 
+    // delete...
+    // --> This is also no different to what you've been doing so far.
     @GetMapping("/school/delete")
     public String deleteSchool(@RequestParam Long id) {
 
         // TODO : delete a school
+    	repository.deleteById(id);
 
         return "redirect:/schools";
     }
 }
+
+
+
--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,16 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // TODO : update this entity
-
+@Entity
 public class School {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long capacity;
@@ -11,6 +18,10 @@ public class School {
 
     public School() {
     }
+    
+    // getter and setters (Übersetzung:: omitted for brevity - ausgelassen für die Kürze)
+    
+    // more information below... --> -->
 
     public Long getId() {
         return id;
@@ -44,3 +55,29 @@ public class School {
         this.country = country;
     }
 }
+
+
+
+/* --> Information on the quest:
+
+Here is the mandatory information that any respectable entity must have:
+
+- the annotation @Entity which indicates that your object will be managed by Spring Data 
+  and that its attributes will be converted into columns of DB tables.
+  
+- @Id indicates that the attribute should be used as the primary key in the table corresponding
+  to the entity.
+  
+- @GeneratedValue(strategy = GenerationType.IDENTITY) which indicates that the value of the id
+  will be generated automatically, and incrementally, upon insertion into the database. This
+  annotation can accept various values (see the resources section for more information), depending
+  on the project requirements.
+  
+- An empty constructor as well as all the getters and setters: it is because of them that an
+  instance of the entity can be automatically created by its repository.
+  
+We could have left in the constructor we used in the JDBC quests earlier, but it is no longer
+needed: JPA will use the empty constructor and the setters.
+
+*/
+
